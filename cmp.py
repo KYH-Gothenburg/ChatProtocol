0bb490d5ef38ebea8530db30e5389222181fbb8c
Joakim Wassberg <joakim@arthead.se>
2020-04-30 15:01:41 +0200

    To and from bytes

--- cmp.py
+++ cmp.py
@@ -9,13 +9,18 @@ from Cryptodome.Util.Padding import pad, unpad
 
 
 class ChatHeader:
-    def __init__(self, date_time, version, message_type, body, encryption):
+    def __init__(self, date_time, version, message_type, encryption, body=None, crc=None):
         self.timestamp = self.set_timestamp(date_time)
         self.version = version
         self.message_type = message_type
-        self.crc = self.set_crc(body)
+        if body:
+            self.crc = self.set_crc(body)
+        elif crc:
+            self.crc = crc
+        else:
+            raise ValueError("Must pass either body or crc")
         self.encryption = encryption
-        self.body_length = len(body.body)
+
 
     def set_timestamp(self, date_time):
         return int(datetime.datetime.timestamp(date_time))
@@ -66,11 +71,16 @@ class ChatHeader:
         pt = unpad(cipher.decrypt(ct), AES.block_size)
         return pt.decode('utf-8')
 
+    def __bytes__(self):
+        return repr(self).encode('utf-8')
+
+    def __repr__(self):
+        return f"{self.timestamp}:<>:{self.version}:<>:{self.message_type}:<>:{self.crc}:<>:{self.encryption}:<>:"
 
 class ChatBody:
-    def __init__(self, body=None):
-        self.iv = None
-        self.session_key = None
+    def __init__(self, body=None, iv=None, session_key=None):
+        self.iv = iv
+        self.session_key = session_key
         self.body = body
 
     def copy(self):
@@ -80,40 +90,81 @@ class ChatBody:
         body.body = self.body
         return body
 
+    def __repr__(self):
+        return f"{self.iv}:<>:{self.session_key}:<>:{self.body}"
+
+    def __bytes__(self):
+        return repr(self).encode('utf-8')
+
 
 class ChatMessageProtocol:
-    def __init__(self, header, body):
+    def __init__(self, header: ChatHeader=None, body: ChatBody=None):
         self.header = header
         self.body = body
+
+    def __bytes__(self):
+        header = bytes(self.header)
+        body = bytes(self.body)
+
+        return header + body
+
+    @staticmethod
+    def from_bytes(byte_data: bytes):
+        chat_message_object = ChatMessageProtocol()
+        in_msg = byte_data.decode('utf-8')
+        data = in_msg.split(':<>:')
+        date_time = datetime.datetime.fromtimestamp(int(data[0]))
+        chat_header = ChatHeader(date_time, data[1], int(data[2]), bool(data[4]), crc=data[3])
+        chat_message_object.header = chat_header
+        chat_body = ChatBody(data[5], data[7], data[6])
+        chat_message_object.body = chat_body
+        return chat_message_object
+
 
 
 # Sender client
 cb = ChatBody("hej.")
 
-ch = ChatHeader(datetime.datetime.now(), 1.0, 2, cb, False)
-#ch.encrypt(cb)
+ch = ChatHeader(datetime.datetime.now(), '1.0', 2, True, body=cb)
+if ch.encryption:
+    ch.encrypt(cb)
 chat_package = ChatMessageProtocol(ch, cb)
+
+cp_as_bytes = bytes(chat_package)
+# Send cp_as_bytes
 
 
 # Servern
+# Receive cp_as_bytes
 b_pub = RSA.import_key(open("B_pub.pem").read())
 c_pub = RSA.import_key(open("C_pub.pem").read())
 
-b_cb = chat_package.body.copy()
-c_cb = chat_package.body.copy()
+new_chat_package = ChatMessageProtocol.from_bytes(cp_as_bytes)
 
+# Copy for each receiving client
+b_cb = new_chat_package.body.copy()
+c_cb = new_chat_package.body.copy()
+
+# Re-encrypt session key for each client
 if chat_package.header.encryption:
     ch.room_recrypt(b_cb, b_pub)
     ch.room_recrypt(c_cb, c_pub)
 
+# Create packge for each client
 b_package = ChatMessageProtocol(chat_package.header, b_cb)
 c_package = ChatMessageProtocol(chat_package.header, c_cb)
 
-# Receiver client B
-header = b_package.header
-body = b_package.body
+# Send bytes to each client
+b_bytes = bytes(b_package)
+c_bytes = bytes(c_package)
 
-if header.version == 1.0:
+# Receiver client B
+# Receive b_bytes
+new_b_package = ChatMessageProtocol.from_bytes(b_bytes)
+header = new_b_package.header
+body = new_b_package.body
+
+if header.version == '1.0':
     if header.encryption:
         b_priv = RSA.import_key(open("B_priv.pem").read())
         msg = ch.decrypt(body, b_priv)
@@ -122,9 +173,11 @@ if header.version == 1.0:
         print("Client B:", body.body)
 
 # Receiver client C
-header = c_package.header
-body = c_package.body
-if header.version == 1.0:
+# Receive c_bytes
+new_c_package = ChatMessageProtocol.from_bytes(c_bytes)
+header = new_c_package.header
+body = new_c_package.body
+if header.version == '1.0':
     if header.encryption:
         c_priv = RSA.import_key(open("C_priv.pem").read())
         msg = ch.decrypt(body, c_priv)
